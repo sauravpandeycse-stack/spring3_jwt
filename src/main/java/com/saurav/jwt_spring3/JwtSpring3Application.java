9a97a9a7ce087230eae4b41b0c58b08eb8d5db89
sauravpandey <sauravpandeycs@gmail.com>
2023-10-09 14:20:13 +0530

    initial code

---
+++ src/main/java/com/saurav/jwt_spring3/JwtSpring3Application.java
@@ -0,0 +1,13 @@
+package com.saurav.jwt_spring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpring3Application.class, args);
+	}
+
+}
